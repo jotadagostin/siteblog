d87a18d92d0a3018c2f027482b9e98dfe2b53f85
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2026-03-09 11:00:05 +0100

    new section
    features-section design ready

--- site-blog/src/components/hero-section/hero-section.tsx
+++ site-blog/src/components/hero-section/hero-section.tsx
@@ -9,7 +9,7 @@ export const HeroSection = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 min-h-[20rem] md:h-[36rem] items-center">
         <div className="flex flex-col items-center justify-center gap-4 md:items-start lg:items-start">
           <h1 className="text-gray-100 text-heading-hg">
-            Venda seus produtos como afiliado em um único lugar
+            Sell ​​your products as an affiliate in one place.
           </h1>
 
           <div className="flex flex-col items-center justify-center gap-4 md:items-start lg:items-start">
@@ -17,14 +17,14 @@ export const HeroSection = () => {
               <div className="flex items-center gap-2">
                 <Clock className="text-cyan-100 h-4 w-4" />
                 <span className="text-gray-200">
-                  Crie o seu site em menos de 5 minutos
+                  Create your website in less than 5 minutes.
                 </span>
               </div>
 
               <div className="flex items-center gap-2">
                 <Store className="text-cyan-100 h-4 w-4" />
                 <span className="text-gray-200">
-                  Acompanhe e otimize seu negócio online
+                  Track and optimize your online business
                 </span>
               </div>
             </div>
@@ -32,13 +32,13 @@ export const HeroSection = () => {
             <div className="text-white flex flex-col gap-2 mt-5 items-center md:items-start ls:items-start">
               <Button className="rounded-full w-fit" asChild>
                 <Link href="/criar-loja">
-                  Criar loja grátis
+                  Create your free store
                   <ArrowRight />
                 </Link>
               </Button>
 
               <p className="text-gray-300 text-body-xs">
-                Não precisa de cartão de crédito
+                No credit card required
               </p>
             </div>
           </div>
