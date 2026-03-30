7a7d5fc6faa05062174c195129b8929a72e953a9
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2026-03-30 10:46:05 +0200

    setting metadata

--- site-blog/src/app/blog/[slug]/page.tsx
+++ site-blog/src/app/blog/[slug]/page.tsx
@@ -1,13 +1,33 @@
 import { PostPage } from "@/templates/blog";
 import { allPosts } from "contentlayer/generated";
+import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 
-type BlogPostPage = {
+type BlogPostPageProps = {
   params: Promise<{
     slug: string;
   }>;
 };
+export async function generateMetadata({
+  params,
+}: BlogPostPageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const post = allPosts.find((post) => post.slug === slug);
 
+  if (!post) {
+    return {};
+  }
+
+  return {
+    title: post.title,
+    description: post.description,
+    authors: [{ name: post.author.name }],
+    robots: "index, follow",
+    openGraph: {
+      images: [post.image],
+    },
+  };
+}
 export const revalidade = 60;
 
 export async function generateStaticParams() {
@@ -16,7 +36,7 @@ export async function generateStaticParams() {
   }));
 }
 
-export default async function BlogPostPage({ params }: BlogPostPage) {
+export default async function BlogPostPage({ params }: BlogPostPageProps) {
   const { slug } = await params;
   const post = allPosts.find((post) => post.slug === slug);
 
